a4d973bd71fc440092b643987acb4bd402fef36a
Jack Antico <you@example.com>
2021-08-24 21:07:09 -0400

    first commit

---
+++ contracts/ToDoList.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0; 
+  //state variable, variable stored on blockchain, public then gives us a function to read the variables (provided by solidity)
+}
